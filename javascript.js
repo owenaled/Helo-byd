ebeb39ea1e8dc72f08eedcf662634a09a1c4ecc2
owenaled <oagriff@gmail.com>
2022-07-08 09:05:04 +0100

    updated js

---
+++ javascript.js
@@ -0,0 +1,9 @@
+let myNumber = 14;
+myNumber.toString(32);
+myNumber.toString(16);
+myNumber.toString(12);
+myNumber.toString(10);
+myNumber.toString(8);
+myNumber.toString(2);
+
+console.log(myNumber.toString(16))
